9623b160a3832c2864321b1edc596867f78e9e04
Changqing Jing <changqing.jing@bmw.com>
2023-07-17 15:05:25 +0800

    use typescript project

--- tests/lib/rules/noVar.test.ts
+++ tests/lib/rules/noVar.test.ts
@@ -2,39 +2,48 @@ import { noVarRule } from "../../../lib/rules/noVar.js";
 
 import { RuleTester } from "eslint";
 // eslint-disable-next-line node/no-unpublished-import
-import { test } from "mocha";
+import { test, suite } from "mocha";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
+import { createRequire } from "module";
 
 const currentFilePath = fileURLToPath(import.meta.url);
 const projectRoot = dirname(dirname(dirname(dirname(dirname(currentFilePath)))));
-const typeScriptParserPath = join(projectRoot, "node_modules", "@typescript-eslint", "parser", "dist", "parser.js");
+const require = createRequire(import.meta.url);
+
+// Resolve the absolute path of @typescript-eslint/parser
+const parserPath = require.resolve("@typescript-eslint/parser");
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
-
-test("test wrong input path", () => {
-  const ruleTester = new RuleTester({
-    parser: typeScriptParserPath,
-    parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      tsconfigRootDir: projectRoot,
-    },
-  });
-  ruleTester.run("no-var", noVarRule, {
-    valid: [
-      {
-        code: "let a:number = 1;",
+suite("test no var lint", () => {
+  test("test no var lint", () => {
+    const ruleTester = new RuleTester({
+      parser: parserPath,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        tsconfigRootDir: projectRoot,
+        project: ["./tsconfig.eslint.json"],
       },
-    ],
+    });
+    const tempFileName = join(projectRoot, "tests", "temp.ts");
+    ruleTester.run("no-var", noVarRule, {
+      valid: [
+        {
+          code: "let a:number = 1;",
+          filename: tempFileName,
+        },
+      ],
 
-    invalid: [
-      {
-        code: "var a:number = 1;",
-        errors: [{ message: "not use var", type: "VariableDeclaration" }],
-        output: "let a:number = 1;",
-      },
-    ],
+      invalid: [
+        {
+          code: "var a:number = 1;",
+          errors: [{ message: "not use var", type: "VariableDeclaration" }],
+          output: "let a:number = 1;",
+          filename: tempFileName,
+        },
+      ],
+    });
   });
 });
